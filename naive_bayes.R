eada202298f6b5fde088717debfc0eab106cfab3
Jonas Mielck <jonasmielck@Jonass-MBP.fritz.box>
2022-07-01 11:33:31 +0200

    naive bayes classifier works, now create training and test data :-(

--- naive_bayes.R
+++ naive_bayes.R
@@ -1,27 +1,46 @@
+#joni: setwd("~/Documents/dev/business/bonito")
+
 require(quanteda)
 require(quanteda.textmodels)
 require(caret)
+library(readr)
 
-# generate 1500 numbers without replacement
+data = read.csv("data/labelled_data.csv")
+
+
 set.seed(73)
-id_train <- sample(1:2000, 1500, replace = FALSE)
-head(id_train, 10)
 
-# create docvar with ID
-data$id <- 1:ndoc(data)
+# rename column so that we have a text number 
+names(data)[names(data) == "review_body"] = "text"
+corpus_resending = corpus(data)
+
 
 # tokenize texts
-toks <- tokens(data, remove_punct = TRUE, remove_number = TRUE) %>% 
+toks = tokens(corpus_resending, remove_punct = TRUE, remove_number = TRUE) %>% 
   tokens_remove(pattern = stopwords("de")) %>% 
   tokens_wordstem()
-dfmt <- dfm(toks)
+dfmt = dfm(toks)
+dfmt
+toks
 
 # get training set
-dfmat_training <- dfm_subset(dfmt, id %in% id_train)
+dfmat_training = dfmt[1:20,]
 
 # get test set (documents not in id_train)
-dfmat_test <- dfm_subset(dfmt, !id %in% id_train)
+dfmat_test = dfmt[21:nrow(dfmt),]
+
+dfmat_test
 
 # train classifier 
-tmod_nb <- textmodel_nb(dfmat_training, dfmat_training$label)
-summary(tmod_nb)
+model_nb = textmodel_nb(dfmat_training, dfmat_training$topic)
+summary(model_nb)
+
+# check result accuracy
+dfmat_matched = dfm_match(dfmat_test, features = featnames(dfmat_training))
+
+actual_class = dfmat_matched$topic
+predicted_class = predict(model_nb, newdata = dfmat_matched)
+tab_class = table(actual_class, predicted_class)
+tab_class
+
+confusionMatrix(tab_class, mode = "everything")

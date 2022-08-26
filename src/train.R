726f0c3a807c337cbd6f98459df2ad44d4523b63
Jonas Mielck <jonasmielck@Jonass-MBP.fritz.box>
2022-08-26 13:28:15 +0200

    removed shoe category from .csv, removed column renaming from code

--- src/train.R
+++ src/train.R
@@ -3,21 +3,14 @@ library(quanteda.textmodels)
 library(caret)
 library(readr)
 
-# readme: set local working directory in RStudio
+
 # TODO: change to your filename
 data = read.csv("data/aws_dataset_de_train_subset.csv", sep=";")
-
-set.seed(73)
-
-# rename column to be able to build corpus object [remove from script and 
-# perform one time and include it in dataset. Also remove "shoe" column.]
-names(data)[names(data) == "review_body"] = "text"
 
 # build corpus object 
 corpus_resending = corpus(data)
 
 # tokenize texts
-# readme: If working with another language change "de" to language of choice (link for everyone to check)
 # TODO: Change "de" to the language of your training data
 toks = tokens(corpus_resending, remove_punct = TRUE, remove_number = TRUE) %>% 
   tokens_remove(pattern = stopwords("de")) %>% 
